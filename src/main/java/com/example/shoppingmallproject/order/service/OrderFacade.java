16e3d74d307174d2f85080e96e0df210bc9888ca
dangddoong <dangddoong@gmail.com>
2023-08-17 19:09:49 +0900

    Refactor: OrderFacade refactor

--- src/main/java/com/example/shoppingmallproject/order/service/OrderFacade.java
+++ src/main/java/com/example/shoppingmallproject/order/service/OrderFacade.java
@@ -5,7 +5,6 @@ import com.example.shoppingmallproject.user.entity.User;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
@@ -19,21 +18,29 @@ public class OrderFacade {
   private final OrderService orderService;
 
   public Long createOrderWithDistributedLock(OrderRequestDto dto, User user) {
-    List<Long> productIds = dto.getProductIds();
-    List<RLock> locks = productIds.stream()
-        .map(productId -> redissonClient.getLock("productId:" + productId))
-        .toList();
+    List<RLock> locks = getRLocks(dto);
     try {
-      for (RLock lock : locks) {
-        boolean available = lock.tryLock(11, 1, TimeUnit.SECONDS);
-        if (!available)
-          throw new IllegalArgumentException();
-      }
+      tryRLocks(locks);
       return orderService.createOrder(dto, user);
     } catch (InterruptedException e) {
-      throw new RuntimeException(e);
+      throw new RuntimeException(e); // Custom Exception을 만들어서 던져주면 더 좋겠지유
     } finally {
       locks.forEach(Lock::unlock);
     }
+  }
+
+  private void tryRLocks(List<RLock> locks) throws InterruptedException {
+    for (RLock lock : locks) {
+      boolean available = lock.tryLock(10, 1, TimeUnit.SECONDS);
+      if (!available)
+        throw new IllegalArgumentException();
+    }
+  }
+
+  private List<RLock> getRLocks(OrderRequestDto dto) {
+    List<Long> productIds = dto.getProductIds();
+    return productIds.stream()
+        .map(productId -> redissonClient.getLock("productId:" + productId))
+        .toList();
   }
 }
